45cac1d40ca18ebbfb8d9d65c3a2088ef6c4527c
joasasso <joasasso@gmail.com>
2025-07-01 16:54:29 -0300

    Changed package name from com.example.minitoolbox to com.joasasso.minitoolbox

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.minitoolbox"
+    namespace = "com.joasasso.minitoolbox"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.minitoolbox"
+        applicationId = "com.joasasso.minitoolbox"
         minSdk = 28
         targetSdk = 36
         versionCode = 1
