fa668cc3c76a25b475c21720b562fc8862d28ce2
joasasso <joasasso@gmail.com>
2025-10-18 20:56:55 -0300

    build: Increment version to 1.1.4
    This commit updates the app's version information in preparation for a new release.
    
    **Key Changes:**
    
    - **App Version (`app/build.gradle.kts`):**
      - The `versionName` has been updated from "1.1.3" to "1.1.4".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 10
-        versionName = "1.1.3"
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
