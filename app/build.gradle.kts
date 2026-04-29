0c82964dc73df7f17412703ea8106fbcdc2d6647
joasasso <joasasso@gmail.com>
2026-04-29 18:32:11 -0300

    build: increment version to 1.2.5
    - Updated `versionCode` from 18 to 19.
    - Updated `versionName` from "1.2.4" to "1.2.5".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.joasasso.minitoolbox"
         minSdk = 28
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.2.4"
+        versionCode = 19
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
